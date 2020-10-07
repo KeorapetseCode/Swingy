780ea401f46f3ebb2c6891cb01e5524acc297354
KeorapetseCode <kmpoloke@student.wethinkcode.co.za>
2020-10-07 16:26:48 +0200

    done!

--- src/controller/ConsoleMode.java
+++ src/controller/ConsoleMode.java
@@ -16,53 +16,79 @@ public class ConsoleMode {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         // Reading data
         try {
-            String input = reader.readLine();
-            if (input.equals("1")){
-                while (true){
-                    System.out.println("Give Player Name" + "\n"); 
-                    input = null;
-                    input = reader.readLine();
-                    if (input.isEmpty() == false){
-                        break ;
-                    }
-                }
-                String playerName = input;
-                Character playerObj = new Character();
-                input = null;
-                while(true){
-                    System.out.println("What Type of Hero Is Your Player" + "\n\n" + "1. Warrior" + "\n" + "2. Assassin" + "\n" + "3. Brute");  
-                    input = reader.readLine();
-                    if (input.isEmpty() == false){
-                        if(input.equals("1") || input.equals("2") || input.equals("3")){
+            Boolean loop = true;
+            while (loop){
+                String input = reader.readLine();
+                if (input.equals("1")){
+                    while (true){
+                        System.out.println("Give Player Name" + "\n"); 
+                        input = null;
+                        input = reader.readLine();
+                        if (input.isEmpty() == false){
+                            if (input.equals("exit") == true){
+                                reader.close();
+                                System.exit(0);
+                            }
                             break ;
                         }
                     }
-                }
-                if (input.equals("1")){
-                    playerObj.createPlayer(playerName, "Warrior");
+                    String playerName = input;
+                    Character playerObj = new Character();
+                    input = null;
+                    while(true){
+                        System.out.println("What Type of Hero Is Your Player" + "\n\n" + "1. Warrior" + "\n" + "2. Assassin" + "\n" + "3. Brute");  
+                        input = reader.readLine();
+                        if (input.isEmpty() == false){
+                            if(input.equals("1") || input.equals("2") || input.equals("3")){
+                                break ;
+                            }
+                            else if (input.equals("exit")){
+                                reader.close();
+                                System.exit(0);
+                            }
+                            else{
+                                System.out.println("Unrecognised Input");
+                            }
+                        }
+                    }
+                    if (input.equals("1")){
+                        playerObj.createPlayer(playerName, "Warrior");
+                    }
+                    else if (input.equals("2")){
+                        playerObj.createPlayer(playerName, "Assassin");
+                    }
+                    else if (input.equals("3")){
+                        playerObj.createPlayer(playerName, "Brute");
+                    }
+                    Map.newMap(playerObj);
+                    SaveGame.createSaveFile(playerObj);
+                    Gameplay.iterate(playerObj);
+                    loop = false;
                 }
                 else if (input.equals("2")){
-                    playerObj.createPlayer(playerName, "Assassin");
-                }
-                else if (input.equals("3")){
-                    playerObj.createPlayer(playerName, "Brute");
-                }
-                Map.newMap(playerObj);
-                SaveGame.createSaveFile(playerObj);
-                Gameplay.iterate(playerObj);
-            }
-            else if (input.equals("2")){
-                while (true){
-                    System.out.println("Give Player Name" + "\n"); 
-                    input = null;
-                    input = reader.readLine();
-                    if (input.isEmpty() == false){
-                        break ;
+                    while (true){
+                        System.out.println("Give Player Name" + "\n"); 
+                        input = null;
+                        input = reader.readLine();
+                        if (input.isEmpty() == false){
+                            if (input.equals("exit")){
+                                reader.close();
+                                System.exit(0);
+                            }
+                            break ;
+                        }
                     }
+                    SaveGame.loadPlayer(input);
+                    loop = false;
                 }
-                SaveGame.loadPlayer(input);
-            }
-            else if (input.equals("exit")){
+                else if (input.equals("exit")){
+                    loop = false;
+                    System.exit(0);
+                }
+                else{
+                    System.out.println("Unrecognised Input");
+                    //line = null;
+                }
             }
         }
         catch (IOException err) {
